33544032220f5f82a85f1ed193b52019efb21d36
Ihor Sudenko <sudenkoweb@gmail.com>
2021-12-15 08:44:31 +0200

    delete unused files & logics

--- source/server.js
+++ source/server.js
@@ -5,18 +5,13 @@ import mongoose from 'mongoose';
 import connectMongo from 'connect-mongo';
 import bodyParser from 'body-parser';
 import dg from 'debug';
-import passport from 'passport';
 import helmet from 'helmet';
 
 // Instruments
 import { getPassword, NotFoundError } from './helpers';
-import './libs/passport.lib';
 
 // Initialize DB connection
 import './db';
-
-// Ititialize Telegram Client
-import './telegram.client';
 
 // Routers
 import { authRouter } from './auth/auth.router';
@@ -24,9 +19,6 @@ import { staffRouter } from './staff/staff.router';
 import { customerRouter } from './customer/customer.router';
 import { productRouter } from './product/product.router';
 import { orderRouter } from './order/order.router';
-
-// Models
-import { UserModel } from './user/user.model';
 
 const app = express();
 const debug = dg('server:init');
@@ -62,8 +54,6 @@ app.use(
 );
 app.use(session(sessionOptions));
 app.use(helmet());
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Router middlewares
 app.use('/api/v1/auth', authRouter);
@@ -71,64 +61,6 @@ app.use('/api/v1/staff', staffRouter);
 app.use('/api/v1/customers', customerRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/orders', orderRouter);
-
-app.get('/', (req, res) => {
-    res.type('.html');
-    res.send(
-        `${
-            !req.session?.passport?.user
-                ? '<a href="/registration/google">google</a>'
-                : `Hi ${req.session.passport.user.emails[0].value}`
-        }`
-    );
-});
-
-app.get('/registration/google', (req, res) => {
-    passport.authenticate('google', {
-        scope: 'email',
-    })(req, res);
-});
-
-app.get('/registration/google/callback', (req, res) => {
-    passport.authenticate('google', { successRedirect: '/' })(req, res);
-});
-
-app.get('/auth-success', async (req, res, next) => {
-    try {
-        if (req.session?.passport?.user) {
-            const userObj = {};
-            userObj.emails = {
-                email: req.session.passport.user.emails[0].value,
-                isPrimary: true,
-            };
-            userObj.avatar = req.session.passport.user.photos[0].value;
-            userObj.socialId = req.session.passport.user.id;
-            userObj.provider = req.session.passport.user.provider;
-            userObj.name = { first: 'test', last: 'test123' };
-
-            req.session.userId = await UserModel.findOne(
-                { socialId: userObj.socialId },
-                { _id: 1 }
-            ).lean();
-            console.log('req.session.userId', req.session.userId);
-            req.session.userId =
-                req.session.userId || (await UserModel.create(userObj))._id;
-
-            res.redirect('/profile');
-            console.log('req.session.userId', req.session.userId);
-
-            return;
-        }
-
-        next('authentication error');
-    } catch (error) {
-        next(error.message);
-    }
-});
-
-app.get('/api/v1/auth/logout', (req, res) => {
-    req.session.destroy(() => res.redirect('/'));
-});
 
 if (process.env.NODE_ENV === 'development') {
     app.use((req, res, next) => {
